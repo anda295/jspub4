0970859d5d091a5dc18bf537c5c9188c39b618a5
AlexandraP <alexandra.petras@tss-yonder.com>
2019-07-08 12:17:11 +0300

    test

--- src/todo/service.js
+++ src/todo/service.js
@@ -1,10 +1,9 @@
 export function createNewTaskElement(taskString) {
-  const listlement = `${'<li>'
+  const listElement = `${'<li>'
     + "<input type='checkbox' /><label>"}${taskString}</label>`
     + '<input type=\'text\' /><button class=\'edit\'>Edit</button>'
     + '<button class=\'delete\'>Delete</button>'
     + '</li>';
-  +'</li>';
   return listElement;
 }
 
