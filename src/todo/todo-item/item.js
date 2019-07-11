a74eeb51021a46e40b366bba8715944c3a1a8d62
AlexandraP <alexandra.petras@tss-yonder.com>
2019-07-11 16:09:30 +0300

    Added lint

--- src/todo/todo-item/item.js
+++ src/todo/todo-item/item.js
@@ -1,12 +1,12 @@
 
 class ToDoItem {
-    createNewTaskElementtask(taskString) {
-        const listElement = `${'<li>'
+  createNewTaskElementtask(taskString) {
+    const listElement = `${'<li>'
             + "<input type='checkbox' /><label>"}${taskString}</label>`
             + '<input type=\'text\' /><button class=\'edit\'>Edit</button>'
             + '<button class=\'delete\'>Delete</button>'
             + '</li>';
-        return listElement;
-    }
+    return listElement;
+  }
 }
 export { ToDoItem as default };
--- src/todo/todoservice.js
+++ src/todo/todoservice.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
 export function getToDos() {
-    return ['Go to the doctor', 'Buy bread', 'Call bank'];
+  return ['Go to the doctor', 'Buy bread', 'Call bank'];
 }
